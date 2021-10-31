a0691b8621668a8c29be094263321a347ec285b8
deneb0618 <techdeneb@gmail.com>
2021-10-31 17:18:53 -0400

    update api url

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,7 +1,7 @@
 import styles from "./App.module.scss";
 const { REACT_APP_CLIENT_ID, REACT_APP_AUTHORIZE_URL, REACT_APP_REDIRECT_URL } =
   process.env;
-const AUTH_URL = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${REACT_APP_REDIRECT_URL}&show_dialog=true&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing`;
+const AUTH_URL = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${REACT_APP_REDIRECT_URL}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing`;
 
 export default function Login() {
   return (
